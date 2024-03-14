85a866233e65e3714b1f651eb24e6f3ea443591d
Homit Dalia <homitdalia59@gmail.com>
2024-03-15 02:23:36 +0530

    Initial Commit

---
+++ CryptoWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoWatcher
+//
+//  Created by Homit Dalia on 15/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoWatcher/CryptoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWatcherApp.swift
+//  CryptoWatcher
+//
+//  Created by Homit Dalia on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
